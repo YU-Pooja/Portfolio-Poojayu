476993103d6092d8e1e5bc4cc6484c0be50c9de7
Pooja YU <yupoojayu@gmail.com>
2026-05-11 21:30:49 +0530

    Update CV download link in Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,7 +22,7 @@ export const Hero = () => {
             <a href="#contact" className={styles.contactBtn}>
               Contact Me
             </a>
-            <a href="https://drive.google.com/file/d/1y9vzUBJCK89awoscGBuP3Ri9aE9U-QyY/view?usp=sharing" className={styles.cvBtn}>
+            <a href="https://drive.google.com/file/d/1YFirC5AJLCLP5Tz9i-doOJ065Wch-2_m/view?usp=sharing" className={styles.cvBtn}>
               Download CV
             </a>
           </div>
